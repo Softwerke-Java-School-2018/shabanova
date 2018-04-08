79538878cfdbebb9c108158b1044ca299a80e2d9
BraveLittleTapok <dinara_92_92@mail.ru>
2018-04-08 21:27:52 +0300

    commit111

--- src/ru/softwerke/model/DevicesList.java
+++ src/ru/softwerke/model/DevicesList.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 public class DevicesList {
     LinkedList<Device> devices = new LinkedList<>();
+
     public void addDevice(Device device){
         devices.add(device);
     }
